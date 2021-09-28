7c8e870345e5d94bc5493bd8a470256b641b2f8d
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-09-27 22:28:25 -0300

    add more tests

--- capture.sh
+++ capture.sh
@@ -2,5 +2,5 @@ i=0
 while [ $i -ne 5 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/fruits/wait/banana/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        curl -o ./datasets/fruits/tests/banana/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
 done
