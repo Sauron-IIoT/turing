2387ec6e6aeb22b82ba8a5a6e79abe09e5407048
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-09-27 22:16:14 -0300

    add more validation data

--- capture.sh
+++ capture.sh
@@ -1,6 +1,6 @@
 i=0
-while [ $i -ne 50 ]
+while [ $i -ne 5 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/fruits/wait/apple/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        curl -o ./datasets/fruits/wait/banana/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
 done
