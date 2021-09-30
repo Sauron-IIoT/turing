de6280e3b5c51c177e3ef5c143fbd7871c272b30
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-09-30 03:51:52 -0300

    add motor and esp32 tests

--- capture.sh
+++ capture.sh
@@ -1,6 +1,6 @@
 i=0
-while [ $i -ne 5 ]
+while [ $i -ne 10 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/fruits/tests/banana/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        curl -o ./datasets/esp32_motor/tests/motor/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
 done
