20ddeeabd3e867d02e0b18fe6547d6a354760e54
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-09-30 23:59:37 -0300

    add more empty samples

--- capture.sh
+++ capture.sh
@@ -1,7 +1,6 @@
 i=0
-while [ $i -ne 300 ]
+while [ $i -ne 100 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/esp32_motor/training/empty/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
-        sleep 1
+        curl -o ./datasets/esp32_motor/tests/empty/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
 done
