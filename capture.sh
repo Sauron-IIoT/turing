579491630d8028b4bb3cc0520c5c245a26512141
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-10-01 01:18:11 -0300

    add more esp32 validation and training samples

--- capture.sh
+++ capture.sh
@@ -1,6 +1,7 @@
 i=0
-while [ $i -ne 400 ]
+while [ $i -ne 100 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/esp32_motor/validation/empty/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        curl -o ./datasets/esp32_motor/validation/esp32/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        sleep 2
 done
