d408198a17b2dabad59a63fa04806c135182bd52
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-09-30 23:42:45 -0300

    change empty class samples

--- capture.sh
+++ capture.sh
@@ -1,6 +1,7 @@
 i=0
-while [ $i -ne 500 ]
+while [ $i -ne 300 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/esp32_motor/tests/empty/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        curl -o ./datasets/esp32_motor/training/empty/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        sleep 1
 done
