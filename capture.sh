a3e2fe7746e2cecbd177079ef4d43f1f1f6da100
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-10-01 00:45:45 -0300

    add more empty validations and training samples

--- capture.sh
+++ capture.sh
@@ -1,6 +1,6 @@
 i=0
-while [ $i -ne 500 ]
+while [ $i -ne 400 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/esp32_motor/tests/empty/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        curl -o ./datasets/esp32_motor/validation/empty/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
 done
