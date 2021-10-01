76564c42fb79acc190465b8b68225f70fbfee60c
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-09-30 22:59:03 -0300

    add empty class training, validation and tests

--- capture.sh
+++ capture.sh
@@ -1,6 +1,6 @@
 i=0
-while [ $i -ne 10 ]
+while [ $i -ne 500 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/esp32_motor/training/esp32/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        curl -o ./datasets/esp32_motor/tests/empty/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
 done
