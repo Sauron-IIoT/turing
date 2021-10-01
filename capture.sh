484f774dee7d877b6e20aee7c37139f2ff196e77
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-10-01 02:24:07 -0300

    add esp32 samples from new camera

--- capture.sh
+++ capture.sh
@@ -1,7 +1,6 @@
 i=0
-while [ $i -ne 100 ]
+while [ $i -ne 500 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/esp32_motor/validation/esp32/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
-        sleep 2
+        curl -o ./datasets/esp32_motor/tests/esp32/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.121/capture
 done
