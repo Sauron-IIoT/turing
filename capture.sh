18b9694edb4e9db0dab67b6eb8281a69b8f739d4
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-09-30 22:48:55 -0300

    add low res esp32 training samples

--- capture.sh
+++ capture.sh
@@ -2,5 +2,5 @@ i=0
 while [ $i -ne 10 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/esp32_motor/tests/esp32/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
+        curl -o ./datasets/esp32_motor/training/esp32/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.243/capture
 done
