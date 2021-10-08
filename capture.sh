eceef0b588a3c1101b5033081721954fd3f8d4df
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-10-08 01:22:02 -0300

    add motor samples

--- capture.sh
+++ capture.sh
@@ -2,5 +2,5 @@ i=0
 while [ $i -ne 5 ]
 do
         i=$(($i+1))
-        curl -o ./datasets/esp32_motor/validation/esp32/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.121/capture
+        curl -o ./datasets/esp32_motor/tests/motor/$(uuidgen | tr "[:upper:]" "[:lower:]").jpg http://192.168.0.121/capture
 done
