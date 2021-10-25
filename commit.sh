d221e8a145cd2da259a9e1156030a6eb25bd14e1
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-10-25 01:42:02 -0300

    more samples

---
+++ commit.sh
@@ -0,0 +1,5 @@
+python3 movie.py
+
+git add .
+git commit -m 'more samples'
+git push -u origin esp32_motor_v3:esp32_motor_v3_result
--- move.py
+++ move.py
@@ -2,10 +2,20 @@ import os
 import random
 import shutil
 
-source = '/Users/murilokakazu/Projects/turing/datasets/esp32_motor/training/motor'
-dest = '/Users/murilokakazu/Projects/turing/datasets/esp32_motor/validation/motor'
-files = os.listdir(source)
-no_of_files = 108
+for clazz in ['esp32', 'motor']:
+    source = f'/Users/murilokakazu/Projects/turing/datasets/esp32_motor/training/{clazz}'
+    dest = f'/Users/murilokakazu/Projects/turing/datasets/esp32_motor/test/{clazz}'
+    files = os.listdir(source)
+    no_of_files = len(files) * 0.05
 
-for file_name in random.sample(files, no_of_files):
-    shutil.move(os.path.join(source, file_name), dest)
+    for file_name in random.sample(files, no_of_files):
+        shutil.move(os.path.join(source, file_name), dest)
+
+for clazz in ['esp32', 'motor']:
+    source = f'/Users/murilokakazu/Projects/turing/datasets/esp32_motor/training/{clazz}'
+    dest = f'/Users/murilokakazu/Projects/turing/datasets/esp32_motor/validation/{clazz}'
+    files = os.listdir(source)
+    no_of_files = len(files) * 0.2
+
+    for file_name in random.sample(files, no_of_files):
+        shutil.move(os.path.join(source, file_name), dest)
