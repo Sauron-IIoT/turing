57cdbce426f4d0010a8b2e905710d12ae9d06ce0
Murilo Kakazu <yuri.kakazu@gmail.com>
2021-10-25 01:50:38 -0300

    separate dataset

--- commit.sh
+++
@@ -1,4 +0,0 @@
-python3 move.py && \
-git add . && \
-git commit -m 'more samples' && \
-git push -f -u origin esp32_motor_v3:esp32_motor_v3_result && git reset head~1 && git add . && git stash
---
+++ merge.sh
@@ -0,0 +1,5 @@
+mv ./datasets/esp32_motor/validation/motor/* ./datasets/esp32_motor/training/motor
+mv ./datasets/esp32_motor/validation/esp32/* ./datasets/esp32_motor/training/esp32
+
+mv ./datasets/esp32_motor/test/motor/* ./datasets/esp32_motor/training/motor
+mv ./datasets/esp32_motor/test/esp32/* ./datasets/esp32_motor/training/esp32
